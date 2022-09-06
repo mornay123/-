127e603cd6fc5c180c891d9c9f9885bae5592f36
杨欣悦 <11490425+yang-mornay@user.noreply.gitee.com>
2022-09-06 18:12:32 +0800

    feat:登陆页面完成布局

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <template>
   <div>
-    <span class="toutiao toutiao-gengduo">ninhao </span>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box">111</div>
+    <router-view></router-view>
   </div>
 </template>
-
-<style lang="less" scoped>
-.box {
-  width: 2.5rem;
-  height: 2.5rem;
-  background-color: green;
-}
-</style>
+<script>
+export default {}
+</script>
+<style lang="less" scoped></style>
